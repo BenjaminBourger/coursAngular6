c19818344de84f987421bf4ed9d3bdab4160130c
bbourger <bbourger@unis.fr>
2019-05-27 14:22:40 +0200

    import de reactiveForm

--- restaurangular/src/app/app.module.ts
+++ restaurangular/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -43,7 +44,8 @@ import {MatDividerModule} from '@angular/material/divider';
     MatTableModule,
     MatPaginatorModule,
     MatSortModule,
-    MatDividerModule
+    MatDividerModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- restaurangular/src/app/reserve/reserve.component.ts
+++ restaurangular/src/app/reserve/reserve.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormGroup, FormBuilder } from '@angular/forms';
 @Component({
   selector: 'app-reserve',
   templateUrl: './reserve.component.html',
   styleUrls: ['./reserve.component.css']
 })
 export class ReserveComponent implements OnInit {
+  reservationForm: FormGroup;
+  date = new Date();
+  timeOptions = ['12:00', '12:30', '13:00', '13:30', '19:00', '19:30', '20:00', '20:30'];
+  peopleOptions = [1, 2, 3, 4, 5, 6, 7, 8];
 
   constructor() { }
 
   ngOnInit() {
+    this.createForm();
   }
 
+  createForm() {
+    
+  }
 }
