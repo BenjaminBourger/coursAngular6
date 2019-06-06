cd417f96c4af6b67a0a9ef10db8461d4a45290dd
bbourger <bbourger@unis.fr>
2019-06-06 17:34:50 +0200

    Filtrer

--- restaurangular/src/app/services/schedule.service.ts
+++ restaurangular/src/app/services/schedule.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { map, tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -10,6 +11,11 @@ export class ScheduleService {
   constructor(private httpClient: HttpClient) { }
 
   search(term): Observable<object> {
-    return this.httpClient.get('assets/schedules.json');
+    return this.httpClient.get('assets/schedules.json')
+                          .pipe(
+                            // tslint:disable-next-line:no-string-literal
+                            map(res => res['events'].filter(evt => evt.title.indexOf(term) > -1)),
+                            tap(filteredEvents => console.log('FilteredEvents : ', filteredEvents))
+                          );
   }
 }
